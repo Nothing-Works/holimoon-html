6cd40ba8540dbfd1d963b731c51d0d55a7a76656
AndySong <andylauszp@gmail.com>
2019-10-20 23:48:05 +1300

    first draft

--- src/js/main.js
+++ src/js/main.js
@@ -98,16 +98,16 @@
     time: 1000
   });
 
-  // Porfolio isotope and filter
+  // destination isotope and filter
   $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+    var destinationIsotope = $('.destination-container').isotope({
+      itemSelector: '.destination-item'
     });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
+    $('#destination-flters li').on( 'click', function() {
+      $("#destination-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+      destinationIsotope.isotope({ filter: $(this).data('filter') });
     });
   });
 
