c6482d80c5cb03bbcab8e8bcaeea31fec26b1fd9
AndySong <andylauszp@gmail.com>
2019-10-21 19:39:15 +1300

    wip

--- src/js/contactform.js
+++ src/js/contactform.js
@@ -1,17 +1,16 @@
 jQuery(document).ready(function($) {
   "use strict";
-
+  const form=$('form.contactForm');
   //Contact
-  $('form.contactForm').submit(function() {
-    var f = $(this).find('.form-group'),
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    var f = form.find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
-
       var i = $(this); // current input
       var rule = i.attr('data-rule');
-
       if (rule !== undefined) {
         var ierror = false; // error flag for current input
         var pos = rule.indexOf(':', 0);
@@ -21,7 +20,6 @@ jQuery(document).ready(function($) {
         } else {
           rule = rule.substr(pos + 1, rule.length);
         }
-
         switch (rule) {
           case 'required':
             if (i.val() === '') {
@@ -57,8 +55,8 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    f.children('textarea').each(function() { // run all inputs
 
+    f.children('textarea').each(function() { // run all inputs
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -88,31 +86,11 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if( ! action ) {
-      action = 'contactform/contactform.php';
+
+    if (ferror) {
+      return;
+    }else{
+      form.submit();
     }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
-
-      }
-    });
-    return false;
   });
-
 });
